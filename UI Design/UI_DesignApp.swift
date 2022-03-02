577ab6eb86f4c677300fa624a568d272049e1255
Hans Lehman <hans.lehman@gmail.com>
2022-03-02 11:28:58 -0500

    Initial Commit

---
+++ UI Design/UI_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_DesignApp.swift
+//  UI Design
+//
+//  Created by Hans Lehman on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct UI_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
